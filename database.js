281863379042b857b01a43291f68555229bff547
james247-eng <danieljamesojoagefu4@gmail.com>
2025-05-10 20:30:51 +0100

    initial commit

---
+++ database.js
@@ -0,0 +1,535 @@
+
+
+const nike_data = [{
+    id:"n1",
+    name:"classic alpha fly",
+    price:565,
+    desc:"description here",
+    img:"images/sneakers/nike/4.png"
+},
+{
+    id:"n2",
+    name:"R90d4",
+    price:700,
+    desc:"description here",
+    img:"images/sneakers/nike/8.png"
+},
+{
+    id:"n3",
+    name:"oblak-1",
+    price:590,
+    desc:"description here",
+    img:"images/sneakers/nike/13.png"
+},
+{
+    id:"n4",
+    name:"cool_sneaker",
+    price:271,
+    desc:"description here",
+    img:"images/sneakers/nike/23.png"
+},
+{
+    id:"n5",
+    name:"daniel_storek",
+    price:660,
+    desc:"description here",
+    img:"images/sneakers/nike/15.png"
+},
+{
+    id:"n6",
+    name:"dark_green_vally",
+    price:235,
+    desc:"description here",
+    img:"images/sneakers/nike/18.png"
+},
+{
+    id:"n7",
+    name:"luis_felipe",
+    price:905,
+    desc:"description here",
+    img:"images/sneakers/nike/23.png"
+},
+{
+    id:"n8",
+    name:"int klinta",
+    price:86,
+    desc:"description here",
+    img:"images/sneakers/nike/21.png"
+},
+{
+    id:"n9",
+    name:"gangana flip",
+    price:123,
+    desc:"description here",
+    img:"images/sneakers/nike/10.jpg"
+},
+{
+    id:"n10",
+    name:"luis",
+    price:102,
+    desc:"description here",
+    img:"images/sneakers/nike/9.webp"
+},
+{
+    id:"n11",
+    name:"hOT sTEPOON",
+    price:200,
+    desc:"description here",
+    img:"images/sneakers/nike/11.jpeg"
+},
+{
+    id:"n12",
+    name:"step up 4R",
+    price:85,
+    desc:"description here",
+    img:"images/sneakers/nike/12.jpeg"
+},
+,
+{
+    id:"nike13",
+    name:"cool_sndgreaker",
+    price:520,
+    desc:"description here",
+    img:"images/sneakers/nike/13.jpeg"
+}
+,
+{
+    id:"nike14",
+    name:"sun diccer",
+    price:100,
+    desc:"description here",
+    img:"images/sneakers/nike/14.jpeg"
+}
+,
+{
+    id:"nike15",
+    name:"sne nation",
+    price:259,
+    desc:"description here",
+    img:"images/sneakers/nike/15.jpeg"
+}
+]
+
+
+// blinks data 
+ 
+
+const blinks_data = [{
+    id:"jacooo",
+    name:"jacot lanfrand",
+    price:565,
+    desc:"description here",
+    img:"images/sneakers/hippie/3.webp"
+},
+{
+    id:"easggblinkMw",
+    name:"elfrandino",
+    price:700,
+    desc:"description here",
+    img:"images/sneakers/hippie/6.webp"
+},
+{
+    id:"fly_to",
+    name:"classic alpha fly",
+    price:1000,
+    desc:"description here",
+    img:"images/sneakers/hippie/9.webp"
+},
+{
+    id:"cool_sne",
+    name:"cool_jacket",
+    price:271,
+    desc:"description here",
+    img:"images/sneakers/hippie/1.jpg"
+},
+{
+    id:"danieljjstorek",
+    name:"daniel_storek",
+    price:660,
+    desc:"description here",
+    img:"images/sneakers/hippie/2.jpg"
+},
+{
+    id:"cosland",
+    name:"cosland",
+    price:565,
+    desc:"description here",
+    img:"images/sneakers/hippie/4.jpeg"
+},
+{
+    id:"luisxfelipe",
+    name:"luisxfelipe",
+    price:565,
+    desc:"description here",
+    img:"images/sneakers/hippie/5.jpeg"
+},
+{
+    id:"elengana",
+    name:"elengana",
+    price:565,
+    desc:"description here",
+    img:"images/sneakers/hippie/1.jpg"
+},
+{
+    id:"metra",
+    name:"metra koxxy",
+    price:565,
+    desc:"description here",
+    img:"images/sneakers/hippie/7.jpeg"
+},
+{
+    id:"luijjj",
+    name:"luis",
+    price:565,
+    desc:"description here",
+    img:"images/sneakers/hippie/8.jpeg"
+},
+{
+    id:"fellokyo",
+    name:"fellokyo rant",
+    price:565,
+    desc:"description here",
+    img:"images/sneakers/nike/23.png"
+},
+{
+    id:"guyinta",
+    name:"guyinta rxaa",
+    price:235,
+    desc:"description here",
+    img:"images/sneakers/nike/21.jpg"
+},
+{
+    id:"nik915",
+    name:"sne nation",
+    price:959,
+    desc:"description here",
+    img:"images/sneakers/nike/15.jpeg"
+},
+{
+    id:"nke15",
+    name:"sne nation",
+    price:259,
+    desc:"description here",
+    img:"images/sneakers/nike/15.jpeg"
+},
+{
+    id:"ike15",
+    name:"sne nation",
+    price:259,
+    desc:"description here",
+    img:"images/sneakers/nike/15.jpeg"
+}]
+const hippie_data = [{
+    id:"jacooo",
+    name:"jacot lanfrand",
+    price:565,
+    desc:"description here",
+    img:"images/sneakers/hippie/3.webp"
+},
+{
+    id:"easgg4blinkMw",
+    name:"elfrandino",
+    price:700,
+    desc:"description here",
+    img:"images/sneakers/hippie/6.webp"
+},
+{
+    id:"fly_4to",
+    name:"classic alpha fly",
+    price:1000,
+    desc:"description here",
+    img:"images/sneakers/hippie/9.webp"
+},
+{
+    id:"cool_4sne",
+    name:"cool_jacket",
+    price:271,
+    desc:"description here",
+    img:"images/sneakers/hippie/1.jpg"
+},
+{
+    id:"danie4ljjstorek",
+    name:"daniel_storek",
+    price:660,
+    desc:"description here",
+    img:"images/sneakers/hippie/2.jpg"
+},
+{
+    id:"cos4land",
+    name:"cosland",
+    price:565,
+    desc:"description here",
+    img:"images/sneakers/hippie/4.jpeg"
+},
+{
+    id:"luisx4felipe",
+    name:"luisxfelipe",
+    price:565,
+    desc:"description here",
+    img:"images/sneakers/hippie/5.jpeg"
+},
+{
+    id:"el4eng4ana",
+    name:"elengana",
+    price:565,
+    desc:"description here",
+    img:"images/sneakers/hippie/1.jpg"
+},
+{
+    id:"4met4ra",
+    name:"metra koxxy",
+    price:565,
+    desc:"description here",
+    img:"images/sneakers/hippie/7.jpeg"
+},
+{
+    id:"l4ui4jjj",
+    name:"luis",
+    price:565,
+    desc:"description here",
+    img:"images/sneakers/hippie/8.jpeg"
+},
+{
+    id:"fel4lo4kyo",
+    name:"fellokyo rant",
+    price:565,
+    desc:"description here",
+    img:"images/sneakers/nike/23.png"
+},
+{
+    id:"guy4in4ta",
+    name:"guyinta rxaa",
+    price:235,
+    desc:"description here",
+    img:"images/sneakers/nike/21.jpg"
+},
+{
+    id:"n4ik5915",
+    name:"sne nation",
+    price:959,
+    desc:"description here",
+    img:"images/sneakers/nike/15.jpeg"
+},
+{
+    id:"nk515",
+    name:"sne nation",
+    price:259,
+    desc:"description here",
+    img:"images/sneakers/nike/15.jpeg"
+},
+{
+    id:"ih1ke15",
+    name:"sne nation",
+    price:259,
+    desc:"description here",
+    img:"images/sneakers/nike/15.jpeg"
+}]
+
+
+// ADIDAS PRODUCT DATAS HERE 
+
+
+const adidas_data = [{
+    id:"figftrst",
+    name:"name here",
+    price:565,
+    desc:"description here",
+    img:"images/sneakers/adidas/1.jpg"
+},
+{
+    id:"easblcsinkMw",
+    name:"name here",
+    price:700,
+    desc:"description here",
+    img:"images/sneakers/adidas/9.jpg"
+},
+{
+    id:"clasegsic",
+    name:"classic alpha fly",
+    price:1000,
+    desc:"description here",
+    img:"images/sneakers/adidas/4.jpg"
+},
+{
+    id:"cool_sndgreaker",
+    name:"cool_sneaker",
+    price:271,
+    desc:"description here",
+    img:"images/sneakers/adidas/2.webp"
+},
+{
+    id:"daniuyyel_storek",
+    name:"daniel_storek",
+    price:660,
+    desc:"description here",
+    img:"images/sneakers/adidas/5.jpeg"
+},
+{
+    id:"darkhh_vile",
+    name:"dark_green_vally",
+    price:565,
+    desc:"description here",
+    img:"images/sneakers/adidas/6.jpeg"
+},
+{
+    id:"l_felipe",
+    name:"luis_felipe",
+    price:565,
+    desc:"description here",
+    img:"images/sneakers/adidas/10.jpeg"
+},
+{
+    id:"linta",
+    name:"int klinta",
+    price:565,
+    desc:"description here",
+    img:"images/sneakers/adidas/11.jpeg"
+},
+{
+    id:"taaa",
+    name:"gangana flip",
+    price:565,
+    desc:"description here",
+    img:"images/sneakers/adidas/13.jpeg"
+},
+{
+    id:"muuu",
+    name:"luis",
+    price:565,
+    desc:"description here",
+    img:"images/sneakers/adidas/12.jpG"
+},
+
+,
+{
+    id:"ni16",
+    name:"sneaker22",
+    price:500,
+    desc:"description here",
+    img:"images/sneakers/nike/12.jpeg"
+}
+,
+{
+    id:"flokyo",
+    name:"fellokyo rant",
+    price:565,
+    desc:"description here",
+    img:"images/sneakers/nike/23.png"
+}
+,
+{
+    id:"uuyuyc",
+    name:"classic alpha fly",
+    price:15600,
+    desc:"description here",
+    img:"images/sneakers/adidas/4.jpg"
+},
+{
+    id:"firtt_Pro",
+    name:"white T-shirt",
+    desc:" nice cool T-shirt from Easblink_com",
+    price:56,
+    img:"images/sneakers/puma/1.jpg"
+},{
+    id:"secsasaovyv",
+    name:"white T-shirt",
+    desc:" nice cool T-shirt from part one",
+    price:23,
+    img:"images/sneakers/puma/10.webp"
+}]
+
+
+const puma_data = [{
+    id:"fi1rst_Pro",
+    name:"white T-shirt",
+    desc:" nice cool T-shirt from Easblink_com",
+    price:56,
+    img:"images/sneakers/puma/1.jpg"
+},{
+    id:"se2condt_Pro",
+    name:"white T-shirt",
+    desc:" nice cool T-shirt from part one",
+    price:23,
+    img:"images/sneakers/puma/10.webp"
+},{
+    id:"thi3rd_Pro",
+    name:"white T-shirt",
+    desc:" nice cool complete men's gear",
+    price:19,
+    img:"images/sneakers/puma/3.jpg"
+},{
+    id:"fou4rt_tPro",
+    name:"white T-shirt",
+    desc:" complete set of quality men's shirts",
+    price:76,
+    img:"images/sneakers/puma/2.webp"
+},{
+    id:"fif5th_Pro",
+    name:"white T-shirt",
+    desc:" nice cool T-shirt from Easblink_com",
+    price:56,
+    img:"images/sneakers/puma/4.jpeg"
+},{
+    id:"nin9t_Pro",
+    name:"white T-shirt",
+    desc:" nice cool T-shirt from Easblink_com",
+    price:56,
+    img:"images/sneakers/puma/8.jpeg"
+},
+{
+    id:"second_first_Pro",
+    name:"white T-shirt",
+    desc:" nice cool-T-shirt-from Easblink_com",
+    price:56,
+    img:"images/sneakers/puma/9.webp"
+},{
+    id:"eitht_Pro",
+    name:"white T-shirt",
+    desc:" nice cool T-shirt from Easblink_com",
+    price:56,
+    img:"images/images/sneakers/puma/7.jpeg"
+},{
+    id:"nint_Pro",
+    name:"white T-shirt",
+    desc:" nice cool T-shirt from Easblink_com",
+    price:56,
+    img:"images/sneakers/puma/8.jpeg"
+},
+{
+    id:"second_first_Pro",
+    name:"white T-shirt",
+    desc:" nice cool-T-shirt-from Easblink_com",
+    price:56,
+    img:"images/sneakers/puma/9.webp"
+},{
+    id:"t_Pro",
+    name:"juxica",
+    desc:" nice cool T-shirt from Easblink_com",
+    price:56.9,
+    img:"images/sneakers/puma/5.jpeg"
+},{
+    id:"sev_Pro",
+    name:"white T-shirt",
+    desc:" link_com",
+    price:56,
+    img:"images/sneakers/puma/6.jpeg"
+},{
+    id:"ei_Pro",
+    name:"white T-shirt",
+    desc:" pumam",
+    price:56,
+    img:"images/sneakers/puma/7.jpeg"
+},{
+    id:"n1",
+    name:"white T-shirt",
+    desc:" puma",
+    price:56,
+    img:"images/sneakers/puma/8.jpeg"
+},
+{
+    id:"second_firstPro",
+    name:"pro name",
+    desc:" puma",
+    price:56,
+    img:"images/sneakers/puma/9.webp"
+}]
